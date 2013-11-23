fca52693440efb9e4388a238189fe7c4864ed4fb
Cristiana Cianfagna <cristiana.cianfagna@gmail.com>
2013-11-23 23:31:00 +0100

    main ridotto al minimo

--- main.c
+++ main.c
@@ -32,12 +32,9 @@ cava* cerca_cava(int cava_x, int cava_y, cava *testa){
 	for (p=testa; p != NULL; p=p->next) {
 		if (p->x == cava_x & p->y==cava_y) {
 			ret=p;
-		}	
-			
-			
+		}
 	}
 	return ret;
-		
 }
 
 //inserimento in testa del blocco
@@ -48,16 +45,13 @@ blocco* insert_blocco(int dim_x, int dim_y, int dim_z, int pos_x0, int pos_y0, b
 	/* *primo_blocco �� il puntatore al primo blocco*/
 	/*ritorna la nuova testa della lista di blocchi (quindi il puntatore al primo blocco)*/
 	blocco *nuovo_el;
-	//printf ("Il campo blocco della cava punta a %p\n", primo_blocco->next);
-	nuovo_el=malloc(sizeof(blocco));	
-	
+	nuovo_el=malloc(sizeof(blocco));
 	nuovo_el -> x0 = pos_x0;
 	nuovo_el -> y0 = pos_y0;
 	nuovo_el -> x = dim_x;
 	nuovo_el -> y = dim_y;
 	nuovo_el -> z = dim_z;
 	nuovo_el -> next=primo_blocco;
-
 	return nuovo_el;
 }
 
@@ -111,18 +105,12 @@ blocco* nuovo_blocco(int cava_x, int cava_y, int dim_x, int dim_y, int dim_z, in
 					c->block=insert_blocco(dim_x, dim_y, dim_z, pos_x0, pos_y0, c->block);
 					//printf ("Il campo blocco della cava punta a %p\n", c->block);
 					ret=c->block;
-
-					//printf ("Il valore di ritorno  punta a %p\n", ret);
 				}	
 			}
 		}					
 	}
-
 	return ret;	
-	
 }
-
-
 
 //inserimento in testa della cava
 cava* insert_cava(int cava_x, int cava_y, cava *testa) {
@@ -161,20 +149,19 @@ void printList_B(cava *testa){
 void printList_C(cava *testa){
 	cava *p;
 	p=testa;
-	printf("Il territorio �� composto dalle cave seguenti:\n");
+	printf("Il territorio è composto dalle cave seguenti:\n");
 	for (p=testa; p != NULL; p = p->next){
 		printf(" %d %d\n", p->x, p->y );
 	}
-	
+	printf("\n");
 }
 
 //creo una nuova cava nella lista passandogli la testa della lista e le coordinate
 cava* nuova_cava(int cava_x, int cava_y, cava *t){
 	cava *p;
 	int esiste=0;
-	//scorro la lista per vedere se esiste gi�� una cava con coordinata x=a con quelle coordinate
+	//scorro la lista per vedere se esiste gia' una cava con coordinata x=a con quelle coordinate
 	for (p=t; p != NULL; p=p->next) {
-		//printf("%d", p->x);
 		//se esiste la sostituisco 
 		if ((p->x) == cava_x) {
 			(p->y) = cava_y;
@@ -183,7 +170,6 @@ cava* nuova_cava(int cava_x, int cava_y, cava *t){
 	}
 	//se non esiste la inserisco
 	if (!esiste){
-		//printList_C(t);
 		t=insert_cava(cava_x, cava_y, t);	
 	}	
 	return t;	
@@ -332,6 +318,9 @@ void siti_cerimoniali (cava *territorio) {
 }
 
 blocco* rotazione(int pos_x0, int pos_y0, cava *territorio, int sito_x, int sito_y, int rotazione) {
+	/*sito_x e sito_y sono le coordinate del sito*/
+	/*pos_x0 e pos_y0 sono le coordinate del blocco*/
+	/*rotazione indica la direzione della rotazione*/
 	cava *t;
 	blocco *attuale;
 	blocco *nuovo;
@@ -397,13 +386,11 @@ blocco* rotazione(int pos_x0, int pos_y0, cava *territorio, int sito_x, int sito
 					}
 				}
 			}
-
 		}
-
 	}
-
 	return nuovo;
 }
+
 /*//funzione che verifica l'occupazione delle celle comprese tra x_max e x_min, e tra y_min e y_max e
 //restituisce 1 se occupata o 0 se libera
 int verifica_copertura(cava *sito, int x_min, int x_max, int y_min, int y_max){
@@ -503,21 +490,23 @@ void crea_mappa(cava *territorio) {
 }
 
 
-int leggi_cave(char *nomefile, cave *territorio ){
+int leggi_cave(char *nomefile, cava *territorio ){
 	int *coordinate;
 	int i=0;
 	int length=0;
-	FILE *cave;
 	int result=0;
+	FILE *cave;
 
+	//alloco spazio per array che conterrà le coordinate
 	coordinate=malloc(sizeof(int));
 
 	cave=fopen(nomefile,"r");
+	//se non riesco ad aprire il file
 	if (cave == NULL){
-		printf("Non riesco a leggere il file\n");
 		result=1;
 	}
 	else {
+		//leggo le coordinate e le memorizzo in un array
 		while (fscanf(cave, "%d", &coordinate[i])) {
 			if (coordinate[i]==0)
 				break;
@@ -526,6 +515,7 @@ int leggi_cave(char *nomefile, cave *territorio ){
 		}
 		fclose(cave);
 		length=i;
+		printf("%d\n", length);
 		//creo le cave
 		//i+=2 perche' mi sposto di due alla volta (coppia di coordinate)
 		for(i=0; i<length; i+=2){
@@ -536,82 +526,88 @@ int leggi_cave(char *nomefile, cave *territorio ){
 	return result;
 }
 
+int leggi_blocchi(char *nomefile, cava *territorio ){
+	int *coordinate_b;
+	int i=0;
+	int length=0;
+	FILE *blocchi;
+	int result=0;
+
+	coordinate_b=malloc(sizeof(int));
+
+	printf("Leggo le coordinate dei blocchi dal file blocchi.txt\n");
+	//printf("Inserisci un blocco in una cava. Specifica posizione e dimensione del blocco, e le coordinate della cava terminate da uno zero\n");
+	blocchi=fopen(nomefile,"r");
+	if (blocchi == NULL){
+		printf("Non riesco a leggere il file con i blocchi\n");
+		result=1;
+	}
+	else {
+		i=0;
+		coordinate_b=malloc(sizeof(int));
+		while (fscanf(blocchi, "%d", &coordinate_b[i])) {
+			if (coordinate_b[i]==0)
+				break;
+			i++;
+			coordinate_b=realloc(coordinate_b, (i+1)*sizeof(int));
+		}
+		fclose(blocchi);
+		//length=0;
+		length=i;
+		printf("numero coordinate lette: %d\n", length);
+		//creo i blocchi
+		for (i=0; i<length; i+=7){
+			territorio->block=nuovo_blocco(coordinate_b[i], coordinate_b[i+1], coordinate_b[i+2], coordinate_b[i+3], coordinate_b[i+4], coordinate_b[i+5], coordinate_b[i+6], territorio);
+		}
+	}
+
+	return result;
+
+}
+
 //fare funzione per passare il file rotazione.txt
 
 int main( int argc, char *argv[]) {
-	int i, length;
-	int file=0;
-
-	int *coordinate_b;
+	int file_c=0;
+	int file_b=0;
 	int *mappa;
-
-	FILE *blocchi;
 	cava *territorio;
 	cava *trovato;
-	cava *temp;
 	blocco *ruotato;
-
 	trovato=NULL;
 	territorio=NULL;
-	temp=malloc(sizeof(cava));
 	trovato=malloc(sizeof(cava));
-	i=0;
 
 	//se argc<2 esci
-	if (argc<2){
-		printf("Non hai passato tutti i file richiesti che devono essere almeno due: uno per le cave e uno per i blocchi\n");
-	}
-	else {
+	//if (argc<2){
+		//printf("Non hai passato tutti i file richiesti che devono essere almeno due: uno per le cave e uno per i blocchi\n");
+	//}
+	//else {
+		printf("Leggo le coordinate delle cave dal file %s e creo il territorio\n", argv[1]);
+		//creo le cave leggendo il file passato al main
+		file_c=leggi_cave("cave.txt", territorio);
 
-		//alla funzione leggi cave passerò argcv[1]
-		file=leggi_cave(argv[1], argv[2]);
-		printf("Leggo le coordinate delle cave dal file argv[1]t e creo il territorio\n");
 		//chiamo la funzione
+		if (file_c==0){
+			printf("Ho creato correttamente le cave\n");
+		}
 
+		//stampo le cave che compongno il territorio
+		printList_C(territorio);
 
+		//determino i siti
+		siti_cerimoniali(territorio);
 
-			printf("\n");
-			printList_C(territorio);
-			printf("\n");
+		crea_mappa(territorio);
 
-			//percorrenza_minima(territorio);
-			siti_cerimoniali(territorio);
+		//creo i blocchi
+		file_b=leggi_blocchi("blocchi.txt", territorio);
 
-			printf("Leggo le coordinate dei blocchi dal file blocchi.txt\n");
-			//printf("Inserisci un blocco in una cava. Specifica posizione e dimensione del blocco, e le coordinate della cava terminate da uno zero\n");
-			blocchi=fopen("blocchi.txt","r");
-			if (blocchi == NULL)
-				printf("Non riesco a leggere il file con i blocchi\n");
-			else {
-				i=0;
-				coordinate_b=malloc(sizeof(int));
-				while (fscanf(blocchi, "%d", &coordinate_b[i])) {
-					if (coordinate_b[i]==0)
-						break;
-					i++;
-					coordinate_b=realloc(coordinate_b, (i+1)*sizeof(int));
-				}
-				length=0;
-				length=i;
-				printf("numero coordinate lette: %d\n", length);
-			}
-			fclose(blocchi);
+		//stampo i blocchi
+		printList_B(territorio);
 
-
-	
-	
-			//creo i blocchi
-			for (i=0; i<length; i+=7){
-				territorio->block=nuovo_blocco(coordinate_b[i], coordinate_b[i+1], coordinate_b[i+2], coordinate_b[i+3], coordinate_b[i+4], coordinate_b[i+5], coordinate_b[i+6], territorio);
-			}
-	
-			crea_mappa(territorio);
-
-			//stampo i blocchi
-			printList_B(territorio);
-
-			//ruoto il blocco in posizione 6,7 di dimensione 3,4,5 che si trova nel sito 9,-3 in direzione ovest
-			ruotato=rotazione(6,7,territorio,9,-3, 0);
+		//ruoto il blocco in posizione 6,7 di dimensione 3,4,5 che si trova nel sito 9,-3 in direzione ovest
+		ruotato=rotazione(6,7,territorio,9,-3, 0);
 
 			/*a=0;
 			b=0;
@@ -624,9 +620,6 @@ int main( int argc, char *argv[]) {
 			else
 				printf("Cava non trovata\n");*/
 
-	
-
-		}
-	}
+	//}
 	
 }
